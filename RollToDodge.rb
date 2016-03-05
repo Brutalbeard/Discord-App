68ffc3daf991fd30c9e323ab94e6ae15e4f532c3
Brutalbeard <jceloria@gmail.com>
2016-03-04 17:42:44 -0700

    Removed keeping it from being just me

--- RollToDodge.rb
+++ RollToDodge.rb
@@ -2,8 +2,8 @@ require 'discordrb'
 
 bot = Discordrb::Bot.new "jceloria@icloud.com", "suckit123"
 
-bot.message(from: "BrutalBeard", containing: /\d{1,}[d]\d{1,2}/) do |event|
-  event.respond "Is a stupid idiot!"
+bot.message(containing: /\d{1,}[d]\d{1,2}/) do |event|
+  event.respond "The bot isn't ready you stupid idiot!"
 end
 
 bot.run
