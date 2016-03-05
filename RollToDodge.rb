fe6914b2205f8553d80d94ea968a27d58de82b47
Brutalbeard <jceloria@gmail.com>
2016-03-04 18:05:25 -0700

    Added a second message response

--- RollToDodge.rb
+++ RollToDodge.rb
@@ -6,4 +6,7 @@ bot.message(containing: /\d{1,}[d]\d{1,2}/) do |event|
   event.respond "The bot isn't ready you stupid idiot!"
 end
 
+bot.message(containing: "test") do |event|
+  event.respond "Your test worked."
+end
 bot.run
