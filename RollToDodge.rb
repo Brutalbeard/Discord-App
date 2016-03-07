f48f17c0e8abbc27cc16490646b8fc7086c052ec
Brutalbeard <jceloria@gmail.com>
2016-03-06 23:00:59 -0700

    Moved in our roll
    Moved our roll dealio

--- RollToDodge.rb
+++ RollToDodge.rb
@@ -2,9 +2,9 @@ require 'discordrb'
 
 bot = Discordrb::Commands::CommandBot.new("jceloria@icloud.com", "suckit123", "!")
 
-bot.message(containing: /\d{1,}[d]\d{1,2}/) do |event|
-  event.respond "The bot isn't ready you stupid idiot!"
-end
+#bot.message(containing: /\d{1,}[d]\d{1,2}/) do |event|
+  #event.respond "The bot isn't ready you stupid idiot!"
+  #end
 
 bot.message(containing: "test") do |event|
   event.respond "Your test worked."
@@ -16,8 +16,18 @@ end
 
 bot.command(:hi) { "Hi!" }
 
-bot.command(:roll, description: "Takes a minimum value, and a max value, and gives a random number in between the two.", usage: "Say !roll number number") do |event, min, max|
-  rand(min.to_i .. max.to_i)
-end
+bot.command(:roll) {|event, arg|
+  diceAmount = arg.split("d")[0].to_i
+  diceType = arg.split("d")[1].to_i
+  rolls = Array.new()
+  text = String.new()
+  totRoll = 0
+  for i in 1..(diceAmount)
+    rolls[i] = (rand(1..diceType))
+    totRoll += rolls[i]
+    text << "Roll #{i}: #{rolls[i]} \n"
+  end
+  text << "\nTotal: #{totRoll}"
+event.respond text}
 
 bot.run
