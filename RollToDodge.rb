729595888bf9fa1b737c900b16a220b1bf724149
Brutalbeard <jceloria@gmail.com>
2016-03-06 23:23:26 -0700

    Added a syntax error that points to help

--- RollToDodge.rb
+++ RollToDodge.rb
@@ -1,6 +1,6 @@
 require 'discordrb'
 
-bot = Discordrb::Commands::CommandBot.new("jceloria@icloud.com", "suckit123", "!")
+bot = Discordrb::Commands::CommandBot.new("jceloria@icloud.com", "suckit123", "/")
 
 #bot.message(containing: /\d{1,}[d]\d{1,2}/) do |event|
   #event.respond "The bot isn't ready you stupid idiot!"
@@ -16,18 +16,24 @@ end
 
 bot.command(:hi) { "Hi!" }
 
-bot.command(:roll) {|event, arg|
-  diceAmount = arg.split("d")[0].to_i
-  diceType = arg.split("d")[1].to_i
-  rolls = Array.new()
-  text = String.new()
-  totRoll = 0
-  for i in 1..(diceAmount)
+bot.command(:roll, description: "Returns a roll.", usage: "Type /roll 1d20 as an example") do |event, arg|
+  if arg.match(/\d{1,}[d]\d{1,2}/) == nil
+    text = "Wrong syntax. Try /help roll"
+  else
+    diceAmount = arg.split("d")[0].to_i
+    diceType = arg.split("d")[1].to_i
+    rolls = Array.new()
+    text = String.new()
+    text << "#{event.user.name} rolled #{diceAmount}, #{diceType} sided die...\n"
+    totRoll = 0
+   for i in 1..(diceAmount)
     rolls[i] = (rand(1..diceType))
     totRoll += rolls[i]
     text << "Roll #{i}: #{rolls[i]} \n"
-  end
+   end
   text << "\nTotal: #{totRoll}"
-event.respond text}
+  end
+  text
+end
 
 bot.run
