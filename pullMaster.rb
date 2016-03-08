3d4fc01bfd33a2a71f3226d63b961f9f2b54625d
Fletcher Hancock <fhancock13@gmail.com>
2016-03-07 17:25:00 -0700

    Fletcher updating auto pull

--- pullMaster.rb
+++ pullMaster.rb
@@ -1,9 +1,26 @@
 require "pstore"
 #set h as variable to contain Hash
 h = PStore.new("hash.pstore")
+hashText = String.new("")
+h.transaction do
+  h[:hash] = "test"
+end
 
-if (h != `git rev-parse --verify HEAD`) do |event|
-h = `git rev-parse --verify HEAD`
-`git pull`
-`ruby untitled.rb`
+h.transaction do
+puts "In fetch:"
+hashText = h.fetch(:hash)
+end
+
+puts hashText
+puts "h = #{h}"
+puts "hash = "
+puts `git rev-parse --verify HEAD`
+if (hashText != `git rev-parse --verify HEAD`)
+  print "In main:"
+  h.transaction do
+    h[:hash] = `git rev-parse --verify HEAD`
+  end
+  `git commit -m "automatic pull"`
+  `git pull --rebase master`
+  `ruby RollToDodge.rb`
 end
