a5a8d0c792bff0d8d1974f83f6186cdb11a0dcf1
Fletcher Hancock <fhancock13@gmail.com>
2016-03-07 18:16:50 -0700

    Short version of pullMaster, just using text to test

--- pullMaster.rb
+++ pullMaster.rb
@@ -2,6 +2,8 @@ require "pstore"
 #set h as variable to contain Hash
 h = PStore.new("hash.pstore")
 hashText = String.new("")
+
+#Initializes pstore hash, to verify against current git hash.
 h.transaction do
   if (h.fetch(:hash)=="")
   puts "Initializing"
@@ -9,22 +11,27 @@ h.transaction do
   end
 end
 
+#Gets current Branch Hash from pstore and sets in text variable for comparison
 h.transaction do
 puts "In fetch:"
 hashText = h.fetch(:hash)
 end
 
-puts hashText
-puts "h = #{h}"
-puts "hash = "
+#next lines for troubleshooting only.
+puts "Local Hash = #{hashText}"
+print "Remote hash = "
 puts `git rev-parse --verify HEAD`
-if (hashText != `git rev-parse --verify HEAD`)
-  print "In main:"
-  h.transaction do
-    h[:hash] = `git rev-parse --verify HEAD`
-  end
-  `git stage`
-  `git commit -m "automatic pull"`
-  `git pull --rebase`
-  `ruby RollToDodge.rb`
-end
+
+puts "UPDATING"
+
+
+#if (hashText != `git rev-parse --verify HEAD`)
+#  print "In main:"
+#  h.transaction do
+#    h[:hash] = `git rev-parse --verify HEAD`
+#  end
+#  `git stage`
+#  `git commit -m "automatic pull"`
+#  `git pull --rebase`
+#  `ruby RollToDodge.rb`
+#end
