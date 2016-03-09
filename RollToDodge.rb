f51b5dbe10066cb09b116e55348e765e35357454
Brutalbeard <jceloria@gmail.com>
2016-03-08 21:31:19 -0700

    Hmm

--- RollToDodge.rb
+++ RollToDodge.rb
@@ -79,7 +79,7 @@ bot.command(:define, description: "Defines a word using Urban Dictionary", usage
   event << parse(get_uri(urbandictionary_uri(arg)))['list'].first['definition']}
 
 bot.command(:whoami) {|event|
-  """User Name: #{event.author.name}
+  event << """User Name: #{event.author.name}
   #{uevent.author.status}
   User ID: #{event.author.id}
   Talking in: #{event.author.voice_channel}
