ce4b7763142b3df6974f5e6331d6941bbed5b0d0
Brutalbeard <jceloria@gmail.com>
2016-03-26 23:12:06 -0600

    test to see if update works again

--- pullMaster.rb
+++ pullMaster.rb
@@ -1,3 +1,23 @@
+require "pstore"
+
+#set h as variable to contain Hash
+h = PStore.new("hash.pstore")
+hashText = String.new("")
+
+#Initializes pstore hash, to verify against current git hash.
+h.transaction do
+  if (h.fetch(:hash)=="")
+  puts "Initializing"
+  h[:hash] = "test"
+  end
+end
+
+#Gets current Branch Hash from pstore and sets in text variable for comparison
+h.transaction do
+  hashText = h.fetch(:hash)
+end
+
+
 puts "Updating 50%"
 `git pull`
 puts "Update Complete"
